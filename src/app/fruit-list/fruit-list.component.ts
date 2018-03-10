e204d4812aeb06adeacad6c23519706b6282d266
neeraja83 <neerajaganesh31@gmail.com>
2018-03-10 01:07:49 -0500

    Update fruit-list.component.ts

--- src/app/fruit-list/fruit-list.component.ts
+++ src/app/fruit-list/fruit-list.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FruitService } from './../fruit.service';
+import {NgForm} from '@angular/forms';
+import { Message } from '@angular/compiler/src/i18n/i18n_ast';
 
 @Component({
   selector: 'fruit-list',
@@ -7,35 +9,24 @@ import { FruitService } from './../fruit.service';
 <div class="container-fluid">
   <div class="row">
     <h2>Fruit List</h2>
+    <br><br>
     <h3>{{errorMsg}}</h3>
-   
+   <br>
   <ul *ngFor="let fruit of fruits | orderBy: order">
-      <li>{{fruit.name}} - <b>Quantity Available: </b>{{fruit.quantityAvailable}}</li>
+      {{fruit.name}} - <b>Quantity Available: </b>{{fruit.quantityAvailable}}
       <li>{{fruit.description}}</li>
            
-  <div class="container">
-    <div class="row">
+    <div class="container-fluid">
+    <div class="row book-sb">
 
-        <button class="btn btn-primary" type="submit" (click)="onSave(input.value)">Details</button>
-        <button class="btn btn-primary" type="submit" (click)="onSave(input.value)">Buy</button>
-     <div class="col-lg-4">
-      <div class="input-group">
-        <span class="input-group-btn">
-         <button type="button" class="quantity-left-minus btn btn-default btn-number"  data-type="minus" data-field="">
-         <span class="glyphicon glyphicon-minus"></span>
-         </button>
-        </span>
-        <input type="text" id="quantity" name="quantity" class="form-control input-number" value="10" min="1" max="100">
-        <span class="input-group-btn">
-         <button type="button" class="quantity-right-plus btn btn-default btn-number" data-type="plus" data-field="">
-         <span class="glyphicon glyphicon-plus"></span>
-         </button>
-        </span>
-     </div>
+        <button class="btn btn-primary" type="submit" (click)="onSave(input.value)">Details</button>&nbsp;&nbsp;&nbsp;
+        <button class="btn btn-primary" type="submit" (click)="onSave(input.value)">Buy</button>&nbsp;&nbsp;&nbsp;
+        <input #myInput type="text" value="{{fruit.quantityAvailable}}" style='width:20em'>&nbsp;&nbsp;&nbsp;
+        <button class="btn btn-primary" type="submit" (click)="onClick(myInput.value-1)" style='width:3em'><span class="glyphicon glyphicon-chevron-down"></span></button>
+        
+        </div>
     </div>
-   </div>
-  </div>
-  </ul>
+    </ul>
  </div>
 </div>
   `,
@@ -44,8 +35,16 @@ import { FruitService } from './../fruit.service';
 export class FruitListComponent implements OnInit {
 
   public fruits = [];
+  
   order: string = 'name';
   public errorMsg;
+
+  onClick(value)
+{
+alert(value);
+
+}
+
   constructor(private _fruitService: FruitService) { }
 
   ngOnInit() {
@@ -54,6 +53,4 @@ export class FruitListComponent implements OnInit {
                 error => this.errorMsg = error);
   }
 
-
-
-}
+ }
