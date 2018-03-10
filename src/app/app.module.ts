d126ef935e2ab0b2907ed528bc3b7731d14debaf
neeraja83 <neerajaganesh31@gmail.com>
2018-03-10 01:09:46 -0500

    Update app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,10 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import {FormsModule} from '@angular/forms';
 
 import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 
 import { Ng2OrderModule } from 'ng2-order-pipe';
 
@@ -15,13 +17,13 @@ import { FruitService } from './fruit.service';
   declarations: [
     AppComponent,
     FruitListComponent,
-    FruitDetailComponent
-  ],
+    FruitDetailComponent,
+    ],
   imports: [
     BrowserModule,
     Ng2OrderModule,
-    HttpClientModule
-  ],
+    HttpClientModule,
+    ],
   providers: [FruitService],
   bootstrap: [AppComponent]
 })
